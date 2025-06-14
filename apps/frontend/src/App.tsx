c4111203ecffe3318724749a53e302a2b3bd65bd
Ace Beattie <acebeattie@gmail.com>
2025-06-14 15:58:27 -0400

    use an app layout

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import ExamplePage from './routes/ExamplePage.tsx';
+import Layout from './layouts/AppLayout.tsx';
 
 function App() {
     const router = createBrowserRouter([
         {
-            path: '/',
-            errorElement: <div />,
-            element: <ExamplePage />,
+            Component: Layout,
+            children: [
+                {
+                    index: true,
+                    errorElement: <div />,
+                    Component: ExamplePage,
+                },
+            ],
         },
     ]);
 
--- apps/frontend/src/layouts/AppLayout.tsx
+++ apps/frontend/src/layouts/AppLayout.tsx
@@ -1,14 +1,15 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from '@/components/app-sidebar';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { Outlet } from 'react-router-dom';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
-  )
+export default function Layout() {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <main>
+                <SidebarTrigger />
+                <Outlet />
+            </main>
+        </SidebarProvider>
+    );
 }
